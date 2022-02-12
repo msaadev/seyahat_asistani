9feee9036e229f2c526d467b2e5727b5a78118ab
muhammed samil demir <studi.msa@gmail.com>
2022-02-12 22:49:59 +0300

    select mode vm

--- lib/view/select_mode/view_model/select_mode_viewmodel.dart
+++ lib/view/select_mode/view_model/select_mode_viewmodel.dart
@@ -4,9 +4,11 @@ part 'select_mode_viewmodel.g.dart';
 class SelectModeViewModel = _SelectModeViewModelBase with _$SelectModeViewModel;
 
 abstract class _SelectModeViewModelBase with Store {
+
   @observable
   bool isCar = false;
 
   @action
   void change(bool v) => isCar = v;
+  
 }
